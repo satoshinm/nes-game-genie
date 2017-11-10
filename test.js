a85133f60703e5b000c5b75a8190a06b5fe095d5
Satoshi N. M <snmatsutake@yahoo.co.jp>
2017-11-10 10:18:55 -0800

    Add encoding tests

--- test.js
+++ test.js
@@ -18,3 +18,30 @@ test('decode with key', (t) => {
   t.equal(decode("SLXPLOVS").key, 0xde);
   t.end();
 });
+
+test('encode no key', (t) => {
+  t.equal(encode(0x11d9, 0xad), "SXIOPO");
+  t.equal(decode(encode(0x11d9, 0xad)).address, 0x11d9);
+  t.equal(decode(encode(0x11d9, 0xad)).value, 0xad);
+  t.equal(decode(encode(0x11d9, 0xad)).wantskey, false);
+  t.equal(decode(encode(0x11d9, 0xad)).key, undefined);
+  t.end();
+});
+
+test('encode no key but wants key', (t) => {
+  t.equal(encode(0x11d9, 0xad, undefined, true), "SXSOPO");
+  t.equal(decode(encode(0x11d9, 0xad, undefined, true)).address, 0x11d9);
+  t.equal(decode(encode(0x11d9, 0xad, undefined, true)).value, 0xad);
+  t.equal(decode(encode(0x11d9, 0xad, undefined, true)).wantskey, true);
+  t.equal(decode(encode(0x11d9, 0xad, undefined, true)).key, undefined);
+  t.end();
+});
+
+test('encode with key', (t) => {
+  t.equal(encode(0x1123, 0xbd, 0xde), "SLXPLOVS");
+  t.equal(decode(encode(0x1123, 0xbd, 0xde)).address, 0x1123);
+  t.equal(decode(encode(0x1123, 0xbd, 0xde)).value, 0xbd);
+  t.equal(decode(encode(0x1123, 0xbd, 0xde)).wantskey, true);
+  t.equal(decode(encode(0x1123, 0xbd, 0xde)).key, 0xde);
+  t.end();
+});
